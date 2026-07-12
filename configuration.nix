5f302f9f253d097dc0c6fa24ad34cf51fde25741
matholl <matheusoliveirad2005@gmail.com>
2026-07-12 12:58:12 -0300

    nvim added

--- configuration.nix
+++ configuration.nix
@@ -89,7 +89,7 @@
    # List packages installed in system profile.
     #You can use https://search.nixos.org/ to find more packages (and options).
     environment.systemPackages = with pkgs; [
-     nvim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      git
      curl
